a456550013e30d9ffe2f72c0fa639b4da46f0b59
athulrajts <athulrajts@gmail.com>
2025-02-04 21:20:33 +0530

    try fix publish

--- publish.ps1
+++ publish.ps1
@@ -4,6 +4,3 @@ param(
 
 # build project
 dotnet publish FluentFin\FluentFin.csproj --self-contained -c Release -r win-x64 -o FluentFin\bin\publish\ /property:BuildVersion=$Version
-
-# copy ffmpeg binaries, needed for flyleaf media player
-xcopy Installer\FFmpeg FluentFin\bin\publish\FFmpeg /s /e /h

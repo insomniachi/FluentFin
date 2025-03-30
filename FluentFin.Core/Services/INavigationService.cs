99158701755dd6fa19df3398ec59ca991b83016c
athulrajts <athulrajts@gmail.com>
2025-03-30 21:52:44 +0530

    remove extra spaces

--- FluentFin.Core/Services/INavigationService.cs
+++ FluentFin.Core/Services/INavigationService.cs
@@ -3,9 +3,7 @@
 public interface INavigationServiceCore
 {
 	bool CanGoBack { get; }
-
 	bool NavigateTo(string pageKey, object? parameter = null, bool clearNavigation = false);
-
 	bool GoBack();
 }
 

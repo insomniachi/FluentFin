c3fc4404b7c740a3a083c269b7b60beafe68c95e
Shanu Dey <sd4shanudey@gmail.com>
2025-11-23 08:58:59 +0000

    refactor keyboard media player controls to check app foreground state before executing commands

--- FluentFin/Services/KeyboardMediaPlayerController.cs
+++ FluentFin/Services/KeyboardMediaPlayerController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using DeftSharp.Windows.Input.Keyboard;
 using FluentFin.Core.Contracts.Services;
@@ -12,7 +14,7 @@ namespace FluentFin.Services
 		private readonly Func<Task> _skipSegment;
 		private readonly Action? _toggleFullscreen;
 		private readonly KeyboardListener _listener = new();
-		private readonly List<Guid> _keySubscriptions = [];
+		private readonly List<Guid> _keySubscriptions = new();
 		private readonly TimeSpan _smallSeek = TimeSpan.FromSeconds(5);
 		private readonly TimeSpan _bigSeek = TimeSpan.FromSeconds(10);
 
@@ -31,23 +33,37 @@ namespace FluentFin.Services
 
 		private void SubscribeEvents()
 		{
-			_keySubscriptions.Add(_listener.Subscribe(Key.Space, async () => await TogglePlayPlause()).Id);
+			_keySubscriptions.Add(_listener.Subscribe(Key.Space, async () => { if (IsAppForeground()) await TogglePlayPlause(); }).Id);
 
-			_keySubscriptions.Add(_listener.Subscribe(Key.Left, async() => await SeekTo(_controller.Position - _smallSeek)).Id);
-			_keySubscriptions.Add(_listener.SubscribeCombination([Key.LeftCtrl | Key.RightCtrl, Key.Left], async () => await SeekTo(_controller.Position - _bigSeek)).Id);
+			_keySubscriptions.Add(_listener.Subscribe(Key.Left, async() => { if (IsAppForeground()) await SeekTo(_controller.Position - _smallSeek); }).Id);
+			_keySubscriptions.Add(_listener.SubscribeCombination(new[] { Key.LeftCtrl | Key.RightCtrl, Key.Left }, async () => { if (IsAppForeground()) await SeekTo(_controller.Position - _bigSeek); }).Id);
 
-			_keySubscriptions.Add(_listener.Subscribe(Key.Right, async () => await SeekTo(_controller.Position + _smallSeek)).Id);
-			_keySubscriptions.Add(_listener.SubscribeCombination([Key.LeftCtrl | Key.RightCtrl, Key.Right], async () => await SeekTo(_controller.Position + _bigSeek)).Id);
+			_keySubscriptions.Add(_listener.Subscribe(Key.Right, async () => { if (IsAppForeground()) await SeekTo(_controller.Position + _smallSeek); }).Id);
+			_keySubscriptions.Add(_listener.SubscribeCombination(new[] { Key.LeftCtrl | Key.RightCtrl, Key.Right }, async () => { if (IsAppForeground()) await SeekTo(_controller.Position + _bigSeek); }).Id);
 
-			_keySubscriptions.Add(_listener.Subscribe(Key.S, async () => await _skipSegment()).Id);
+			_keySubscriptions.Add(_listener.Subscribe(Key.S, async () => { if (IsAppForeground()) await _skipSegment(); }).Id);
 
 			if (_toggleFullscreen is not null)
 			{
-				_keySubscriptions.Add(_listener.Subscribe(Key.F, _toggleFullscreen).Id);
+				_keySubscriptions.Add(_listener.Subscribe(Key.F, () => { if (IsAppForeground() && _toggleFullscreen is not null) _toggleFullscreen(); }).Id);
 			}
 
-			_keySubscriptions.Add(_listener.Subscribe(Key.Up, () => _controller.Volume += 1).Id);
-			_keySubscriptions.Add(_listener.Subscribe(Key.Down, () => _controller.Volume -= 1).Id);
+			_keySubscriptions.Add(_listener.Subscribe(Key.Up, () => { if (IsAppForeground()) _controller.Volume += 1; }).Id);
+			_keySubscriptions.Add(_listener.Subscribe(Key.Down, () => { if (IsAppForeground()) _controller.Volume -= 1; }).Id);
+		}
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr GetForegroundWindow();
+
+		[DllImport("user32.dll")]
+		private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+		private static bool IsAppForeground()
+		{
+			var fg = GetForegroundWindow();
+			if (fg == IntPtr.Zero) return false;
+			GetWindowThreadProcessId(fg, out uint pid);
+			return pid == (uint)Process.GetCurrentProcess().Id;
 		}
 
 		public void UnsubscribeEvents()
